8830bb833c1206d6dbe54761ac9436ee684a1d33
WalterGuerra <wsteven00@gmail.com>
2023-11-02 17:34:23 -0400

    add view function and url path for creating a jedi, also add view function to assign stats to jedi based on jedi type

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('jedi/create', views.JediCreate.as_view(), name='jedi-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,22 +7,66 @@ from .models import Jedi
 def home(request):
   return render(request, 'home.html')
 
-class CustomJediCreateView(CreateView):
-    model = Jedi
-    fields = ['name', 'planet', 'age', 'lightsabercolor', 'jeditype']
-    success_url = '/home/'
+class JediCreate(CreateView):
+  model = Jedi
+  fields = ['name', 'planet', 'age', 'lightsabercolor', 'jeditype', 'mentor']
+  success_url = '/home/'
 
-    def form_valid(self, form):
-        # Assign specific stats based on Jedi type before saving the form
-        jedi_type = form.cleaned_data['jeditype']
-        stats = get_jedi_stats(jedi_type)
-        form.instance.powerlevel = stats['power_level']
-        form.instance.lightsaberskill = stats['lightsaber_skills']
-        form.instance.forceabilities = stats['force_abilities']
-        form.instance.defense = stats['defense']
-        form.instance.agility = stats['agility']
-        form.instance.wisdom = stats['wisdom_knowledge']
-        form.instance.stamina = stats['stamina_endurance']
-        form.instance.charisma = stats['charisma_influence']
-        return super().form_valid(form)
+  def form_valid(self, form):
+    jedi_type = form.cleaned_data['jeditype']
+    stats = get_jedi_stats(jedi_type)
+    form.instance.powerlevel = stats['power_level']
+    form.instance.lightsaberskill = stats['lightsaberskills']
+    form.instance.forceabilities = stats['forceabilities']
+    form.instance.defense = stats['defense']
+    form.instance.agility = stats['agility']
+    form.instance.wisdom = stats['wisdom']
+    form.instance.stamina = stats['stamina']
+    form.instance.charisma = stats['charisma']
+    return super().form_valid(form)
 
+def get_jedi_stats(jedi_type):
+  if jedi_type == 'consular':
+    return {
+      'powerlevel': 7,
+      'lightsaberskills': 6,
+      'forceabilities': 9,
+      'defense': 7,
+      'agility': 6,
+      'wisdom': 10,
+      'stamina': 7,
+      'charisma': 8
+    }
+  elif jedi_type == 'sentinel':
+    return {
+      'powerlevel': 8,
+      'lightsaberskills': 8,
+      'forceabilities': 8,
+      'defense': 8,
+      'agility': 7,
+      'wisdom': 8,
+      'stamina': 8,
+      'charisma': 7
+    }
+  elif jedi_type == 'guardian':
+    return {
+      'powerlevel': 9,
+      'lightsaberskills': 9,
+      'forceabilities': 7,
+      'defense': 9,
+      'agility': 7,
+      'wisdom': 7,
+      'stamina': 9,
+      'charisma': 6
+    }
+  else:
+    return {
+      'power_level': 5,
+      'lightsaberskills': 5,
+      'forceabilities': 5,
+      'defense': 5,
+      'agility': 5,
+      'wisdom': 5,
+      'stamina': 5,
+      'charisma': 5
+    }
