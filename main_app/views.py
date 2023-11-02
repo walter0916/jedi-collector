19a9bdaf424dfedca03d07b73191459c2a7038c2
WalterGuerra <wsteven00@gmail.com>
2023-11-02 16:30:22 -0400

    setup docker and base html

---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello</h1>')
+
